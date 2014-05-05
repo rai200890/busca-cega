08927025af9ceafb7a87544eecf31226c76fb153
rai200890 <rai200890@496a62d0-7c6e-6ab0-5dcb-21486d3416c0>
2014-05-05 00:51:18 +0000

    Término do conjunto de estados do problema de torres de hanói.

---
+++ src/buscacega/BuscaLargura.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscacega;
+
+import java.util.LinkedList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+/**
+ *
+ * @author raissa
+ */
+public class BuscaLargura {
+
+    private static void buscar(Problem p) {
+   
+    }
+}
