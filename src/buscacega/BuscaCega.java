712f272882637d265a7bcd23682363d7fd879a73
Robsoncompuff@gmail.com <Robsoncompuff@gmail.com@496a62d0-7c6e-6ab0-5dcb-21486d3416c0>
2014-05-14 05:17:10 +0000

    Implementado a interface para as busca em largura e profundidade

--- src/buscacega/BuscaCega.java
+++ src/buscacega/BuscaCega.java
@@ -14,22 +14,29 @@ public class BuscaCega {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Regra 1
-        BaseConhecimento kb = new BaseConhecimento();
-        HashMap<String, String> transicao = new HashMap<String, String>();
-        transicao.put("x", "y+1");
-        transicao.put("y", "x+2");
-        Transicao t = new Transicao(transicao);
-        kb.addRegra("r1", new PadraoRegra("(x<y) && (x>1)"), t);
-        HashMap<String, BigDecimal> estado = new HashMap<String, BigDecimal>();
-        estado.put("x", new BigDecimal(2));
-        estado.put("y", new BigDecimal(3));
-        HashMap<String, BigDecimal> resultado = new HashMap<String, BigDecimal>();
-        resultado = t.resultado(estado);
-        ArrayList<Regra> filtro = kb.getRegras(estado);
-        for (Regra regra : filtro) {
-            System.out.println(regra.getId());
-            System.out.println(regra.getTransicao().resultado(estado));
-        }
+//        //Regra 1
+//        BaseConhecimento kb = new BaseConhecimento();
+//        HashMap<String, String> transicao = new HashMap<String, String>();
+//        transicao.put("x", "y+1");
+//        transicao.put("y", "x+2");
+//        Transicao t = new Transicao(transicao);
+//        kb.addRegra("r1", new PadraoRegra("(x<y) && (x>1)"), t);
+//        HashMap<String, BigDecimal> estado = new HashMap<String, BigDecimal>();
+//        estado.put("x", new BigDecimal(2));
+//        estado.put("y", new BigDecimal(3));
+//        HashMap<String, BigDecimal> resultado = new HashMap<String, BigDecimal>();
+//        resultado = t.resultado(estado);
+//        ArrayList<Regra> filtro = kb.getRegras(estado);
+//        for (Regra regra : filtro) {
+//            System.out.println(regra.getId());
+//            System.out.println(regra.getTransicao().resultado(estado));
+//        }
+        
+          InterfaceGUI Gui = new InterfaceGUI();
+          Gui.main(args);
+          
+        
+        
+        
     }
 }
